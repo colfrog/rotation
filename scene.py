f2ffb1883dc5a20ce42e21f18a7dd8a38c5ad68b
Laurent Cimon <laurent@nilio.ca>
2019-05-13 10:48:58 -0400

    Change the 3D rotation method

--- scene.py
+++ scene.py
@@ -4,18 +4,29 @@ from mpl_toolkits.mplot3d import Axes3D
 import ipywidgets as widgets
 import numpy as np
 
+def xunit(v: vector = vector()):
+       	return vector([1.0, 0, 0], v.orig)
+def yunit(v: vector = vector()):
+	return vector([0, 1.0, 0], v.orig)
+def zunit(v: vector = vector()):
+	return vector([0, 0, 1.0], v.orig)
+
+
+def make_angle_slider(self):
+	return widgets.IntSlider(min = 0, max = 360, step = 1)
+
 class scene:
-	def __init__(self):
+	def __init__(self, v, d = xunit()):
 		self.fig = plt.figure()
 		self.ax = self.fig.add_subplot(111, projection = "3d", xlim = (-3, 3), ylim = (-3, 3), zlim = (-3, 3))
-		plt.ion()
 
-	def xunit(self, v: vector):
-		return vector([1.0, 0, 0], v.orig)
-	def yunit(self, v: vector):
-		return vector([0, 1.0, 0], v.orig)
-	def zunit(self, v: vector):
-		return vector([0, 0, 1.0], v.orig)
+		self.vi = self.v = v
+		self.di = self.d = d
+		self.Qv = self.quiver(v)
+		self.Qd = self.quiver(d, "green")
+		
+		plt.ion()
+		plt.show()
 
 	def make_angle_slider(self):
 		return widgets.IntSlider(min = 0, max = 360, step = 1)
@@ -25,52 +36,26 @@ class scene:
 		d = v.dest
 		q = self.ax.quiver(o[0], o[1], o[2], d[0], d[1], d[2], color = col)
 		return q
-
-
-class scene1(scene):
-	def __init__(self):
-		super(scene1, self).__init__()
-		self.Q = self.quiver(vector([0, 0, 0]))
-
-	def draw_2D_rotation(self, dir: vector, v: vector, angle):
-		Vrot = v.rotate(dir, angle, True)
-		self.ax.collections.remove(self.Q)
-		self.Q = self.quiver(Vrot)
+	
+	def rotate_vector(self, angle_vecteur):
+		angle = angle_vecteur
+		self.v = self.vi.rotate(self.d, angle, True)
+		self.ax.collections.remove(self.Qv)
+		self.Qv = self.quiver(self.v)
 		self.fig.canvas.draw()
 
-class scene2(scene1):
-	def draw_3D_rotation(self, v: vector, angleX, angleY, angleZ):
-		Vrot = v.rotate(self.xunit(v), angleX) \
-			.rotate(self.yunit(v), angleY) \
-			.rotate(self.zunit(v), angleZ)
-		self.ax.collections.remove(self.Q)
-		self.Q = self.quiver(Vrot)
+	def rotate_dir(self, d, angle_direction):
+		angle = angle_direction
+		self.d = self.di.rotate(d, angle, True)
+		self.ax.collections.remove(self.Qd)
+		self.Qd = self.quiver(self.d, "green")
 		self.fig.canvas.draw()
 
-class scene3(scene):
-	def generate_cube_vectors(self):
-		vectors = []
-		for i in range(-2, 2):
-			for j in range(-2, 2):
-				for k in range(-2, 2):
-					vectors.append(vector([i, j, k]))
-
-		return vectors
-
-	def draw_cube_scatter(self, vectors):
-		x = y = z = []
-		for v in vectors:
-			v.show()
-			x.append(v[0])
-			y.append(v[1])
-			z.append(v[2])
-
-		self.S = self.ax.scatter(x, y, z)
-
-	def rotate_cube(self, dir, vectors, angle, marker = 'o'):
-		vectors_rot = []
-		for v in vectors:
-			vectors_rot.append(v.rotate(dir.unit(), angle, True))
-
-		self.ax.collections.remove(self.S)
-		draw_cube_scatter(vectors_rot)
+#class scene3(scene1):
+#	def draw_3D_rotation(self, v: vector, angleX, angleY, angleZ):
+#		Vrot = v.rotate(self.xunit(v), angleX) \
+#			.rotate(self.yunit(v), angleY) \
+#			.rotate(self.zunit(v), angleZ)
+#		self.ax.collections.remove(self.Q)
+#		self.Q = self.quiver(Vrot)
+#		self.fig.canvas.draw()
